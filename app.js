3798a4a37b5057d69e7f3ec5cce92b980859130c
ZarNiko91 <50491777+ZarNiko91@users.noreply.github.com>
2021-03-11 14:03:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+
+const weatherRoute = require('./routes/weather');
+
+app.set('view engine', 'ejs');
+
+app.use('/', weatherRoute);
+
+
+const PORT = process.env.PORT ||  5000;
+
+app.listen(PORT, () => console.log(`Server starting at port ${PORT}`));
